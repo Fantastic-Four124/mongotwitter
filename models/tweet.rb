5185163281cf47db74cdb7d81f5d7a959232ddbd
vagrant <vagrant@precise64.(none)>
2018-03-07 04:59:27 +0000

    Updated tweet.rb, mongoid.config, service.rb and README.md

--- models/tweet.rb
+++ models/tweet.rb
@@ -1,5 +1,11 @@
 class Tweet
   include Mongoid::Document
 
-  
+  field :tweet_id, type: String
+  field :contents, type: String
+  field :date_posted, type: DateTime
+  field :user_id, type: Integer
+  field :hashtags, type: Array
+  field :mentions, type: Array
+  store_in collection: 'tweets'
 end
--- service.rb
+++ service.rb
@@ -9,3 +9,21 @@ Mongoid.load! "mongoid.config"
 enable :sessions
 
 set :bind, '0.0.0.0' # Needed to work with Vagrant
+
+# These are by no means concrete.
+
+GET '/api/v1/tweets/{:user_id}' do # Get tweets by :user_id
+
+end
+
+GET '/api/v1/tweets/random' do # Get 50 random tweets
+
+end
+
+GET '/api/v1/tweets/{:tweet_id}/hashtags' # Get hashtags associated with the tweet
+
+end
+
+GET '/api/v1/tweets/{:tweet_id}/mentions' # Get mention associated with the tweet
+
+end
