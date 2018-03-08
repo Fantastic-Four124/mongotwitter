5cf4c5b8b799787e8b0546b101b63f7cef4da6d3
vagrant <vagrant@precise64.(none)>
2018-03-08 05:14:01 +0000

    Basic model working

--- service.rb
+++ service.rb
@@ -1,6 +1,7 @@
 # server.rb
 require 'sinatra'
 require 'mongoid'
+require_relative 'models/tweet'
 
 # DB Setup
 Mongoid.load! "mongoid.config"
@@ -12,18 +13,56 @@ set :bind, '0.0.0.0' # Needed to work with Vagrant
 
 # These are by no means concrete.
 
-GET '/api/v1/tweets/{:user_id}' do # Get tweets by :user_id
+# Testing routes only.
+get '/' do
+  erb :home
+end
+
+get '/results' do
+  @text = params[:load]
+  erb :results
+end
+
+# Read the first Tweet that matches the query.
+get '/read' do
+  tweet = Tweet.find_by(user_id: 1)
+  message = "Tweet message: #{tweet.contents}"
+  redirect "/results?load=#{message}"
+end
+
+post '/create' do
+  tweet = Tweet.new(
+    contents: params[:tweet],
+    user_id: 1,
+    date_posted: Time.now,
+    hashtags: ['twitter'],
+    mentions: Array.new
+  )
+  tweet.save!
+  message = "Tweet with #{tweet.id} created."
+  redirect "/results?load=#{message}"
+end
+
+# Delete the first Tweet that matches the query.
+post '/delete' do
+  tweet = Tweet.find_by(user_id: 1)
+  message = "Tweet with #{tweet.id} deleted."
+  tweet.delete
+  redirect "/results?load=#{message}"
+end
+
+get '/api/v1/tweets/:user_id' do # Get tweets by :user_id
 
 end
 
-GET '/api/v1/tweets/random' do # Get 50 random tweets
+get '/api/v1/tweets/random' do # Get 50 random tweets
 
 end
 
-GET '/api/v1/tweets/{:tweet_id}/hashtags' # Get hashtags associated with the tweet
+get '/api/v1/tweets/:tweet_id/hashtags' do # Get hashtags associated with the tweet
 
 end
 
-GET '/api/v1/tweets/{:tweet_id}/mentions' # Get mention associated with the tweet
+get '/api/v1/tweets/:tweet_id/mentions' do # Get mention associated with the tweet
 
 end
